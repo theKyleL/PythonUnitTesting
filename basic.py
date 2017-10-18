a42c08a9313cda8ce369bdb6948b59a1c7251d9f
Kyle Latino <kyle.latino@gmail.com>
2017-10-18 07:30:51 -0700

    minor source adjustment
    Added clarity to pydocs

--- basic.py
+++ basic.py
@@ -3,27 +3,41 @@ This file contains basic python functions for use with the test_basic.py unit te
 """
 
 def basic_addition(a, b):
-	"""Return the sum of the two arguments"""
+	"""
+	Return the sum of the two arguments
+	a + b
+	"""
 	return a + b
 
 
 def basic_subtraction(a, b):
-	"""Return the difference of the two arguments"""
+	"""
+	Return the difference of the two arguments
+	a - b
+	"""
 	return a - b
 
 
 def basic_multiplication(a, b):
-	"""Return the product of the two arguments"""
+	"""
+	Return the product of the two arguments
+	a * b
+	"""
 	return a * b
 
 
 def basic_division(a, b):
-	"""Return the quotient of the two arguments"""
+	"""
+	Return the quotient of the two arguments
+	a / b
+	"""
 	if b == 0:
 		raise ValueError('Division by 0!')
 	return float(a) / float(b)
 
 
 def concatinateStuff(a, b):
-	"""Return the concatinated value"""
+	"""
+	Return the concatinated value
+	"""
 	return '{}{}'.format(a, b)
